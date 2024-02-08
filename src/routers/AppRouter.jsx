684a795a34c093881ca6aef1ec0752fb143db259
alexfang324 <106351391+alexfang324@users.noreply.github.com>
2024-02-07 19:56:37 -0800

    changed website theme to black

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,16 +1,16 @@
 // App Router
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "../components/Header";
-import Nav from "../components/Nav";
-import Footer from "../components/Footer";
-import PageHome from "../pages/PageHome";
-import PageMovieDetails from "../pages/PageMovieDetails";
-import PageAbout from "../pages/PageAbout";
-import PageFavs from "../pages/PageFavs";
-import PageWatchlist from "../pages/PageWatchlist";
-import PageNotFound from "../pages/PageNotFound";
-import { appTitle, appAuthor } from "../globals/globalVariables";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from '../components/Header';
+import Nav from '../components/Nav';
+import Footer from '../components/Footer';
+import PageHome from '../pages/PageHome';
+import PageMovieDetails from '../pages/PageMovieDetails';
+import PageAbout from '../pages/PageAbout';
+import PageFavs from '../pages/PageFavs';
+import PageWatchlist from '../pages/PageWatchlist';
+import PageNotFound from '../pages/PageNotFound';
+import { appTitle, appAuthor } from '../globals/globalVariables';
 
 function AppRouter() {
   return (
@@ -18,17 +18,16 @@ function AppRouter() {
       <div className="wrapper">
         <Header title={appTitle} />
         <Nav />
-        <Routes>
-          <Route path="/" exact element={<PageHome />} />
-          <Route
-            path="/movie-details/:id"
-            element={<PageMovieDetails />}
-          />
-          <Route path="/about" element={<PageAbout />} />          
-          <Route path="/favs" element={<PageFavs />} />
-          <Route path="/watchlist" element={<PageWatchlist />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
+        <main>
+          <Routes>
+            <Route path="/" exact element={<PageHome />} />
+            <Route path="/movie-details/:id" element={<PageMovieDetails />} />
+            <Route path="/about" element={<PageAbout />} />
+            <Route path="/favs" element={<PageFavs />} />
+            <Route path="/watchlist" element={<PageWatchlist />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </main>
         <Footer author={appAuthor} />
       </div>
     </BrowserRouter>
