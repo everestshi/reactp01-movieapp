80e78f3e4effb2d5b3a90bfe7278017be60dfcc3
alexfang324 <106351391+alexfang324@users.noreply.github.com>
2024-02-11 16:14:08 -0800

    added movie banner background to movie detail page

--- src/data/tmdb-data.js
+++ src/data/tmdb-data.js
@@ -1,6 +1,13 @@
-const API_KEY = "be601e74c575148d8ea2224d8cded829";
-const BASE_URL = "https://api.themoviedb.org/3";
-const LANGUAGE = "en-US";
+const API_KEY = 'be601e74c575148d8ea2224d8cded829';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const IMG_BASE_URL = 'https://image.tmdb.org/t/p';
+const LANGUAGE = 'en-US';
+const BANNER_SIZES = [
+  [300, '/w300'],
+  [780, '/w780'],
+  [1280, '/w1280'],
+  [1281, '/original']
+];
 
 // Function to fetch popular movies
 export const fetchPopularMovies = async () => {
@@ -8,12 +15,12 @@ export const fetchPopularMovies = async () => {
   try {
     const response = await fetch(API_URL);
     if (!response.ok) {
-      throw new Error("Network response was not ok");
+      throw new Error('Network response was not ok');
     }
     const data = await response.json();
     return data.results;
   } catch (error) {
-    console.error("Error fetching popular movies:", error);
+    console.error('Error fetching popular movies:', error);
     return [];
   }
 };
@@ -24,12 +31,12 @@ export const fetchTopRatedMovies = async () => {
   try {
     const response = await fetch(API_URL);
     if (!response.ok) {
-      throw new Error("Network response was not ok");
+      throw new Error('Network response was not ok');
     }
     const data = await response.json();
     return data.results;
   } catch (error) {
-    console.error("Error fetching top-rated movies:", error);
+    console.error('Error fetching top-rated movies:', error);
     return [];
   }
 };
@@ -40,12 +47,12 @@ export const fetchNowPlayingMovies = async () => {
   try {
     const response = await fetch(API_URL);
     if (!response.ok) {
-      throw new Error("Network response was not ok");
+      throw new Error('Network response was not ok');
     }
     const data = await response.json();
     return data.results;
   } catch (error) {
-    console.error("Error fetching now playing movies:", error);
+    console.error('Error fetching now playing movies:', error);
     return [];
   }
 };
@@ -56,12 +63,12 @@ export const fetchUpcomingMovies = async () => {
   try {
     const response = await fetch(API_URL);
     if (!response.ok) {
-      throw new Error("Network response was not ok");
+      throw new Error('Network response was not ok');
     }
     const data = await response.json();
     return data.results;
   } catch (error) {
-    console.error("Error fetching upcoming movies:", error);
+    console.error('Error fetching upcoming movies:', error);
     return [];
   }
 };
@@ -75,12 +82,12 @@ export const searchMovies = async (query) => {
   try {
     const response = await fetch(API_URL);
     if (!response.ok) {
-      throw new Error("Network response was not ok");
+      throw new Error('Network response was not ok');
     }
     const data = await response.json();
     return data.results;
   } catch (error) {
-    console.error("Error searching movies by title:", error);
+    console.error('Error searching movies by title:', error);
     return [];
   }
 };
@@ -91,12 +98,12 @@ export const fetchMovieGenres = async () => {
   try {
     const response = await fetch(API_URL);
     if (!response.ok) {
-      throw new Error("Network response was not ok");
+      throw new Error('Network response was not ok');
     }
     const data = await response.json();
     return data.genres;
   } catch (error) {
-    console.error("Error fetching movie genres:", error);
+    console.error('Error fetching movie genres:', error);
     return [];
   }
 };
@@ -107,18 +114,49 @@ export const fetchTrailerUrl = async (id) => {
   try {
     const response = await fetch(API_URL);
     if (!response.ok) {
-      throw new Error("Network response was not ok");
+      throw new Error('Network response was not ok');
     }
     const data = await response.json();
     //filter and return the youtube id for the first one with type Trailer and published on Youtube
     const youtubeId = data.results.filter((obj) => {
-      return obj.type === "Trailer" && obj.site === "YouTube";
+      return obj.type === 'Trailer' && obj.site === 'YouTube';
     })[0].key;
-    return "https://www.youtube.com/embed/" + youtubeId;
+    return `https://www.youtube.com/embed/${youtubeId}?&controls=1&modestbranding=1&playsinline=1&rel=0`;
 
     // return data.results;
   } catch (error) {
-    console.error("Error fetching movie trailer:", error);
+    console.error('Error fetching movie trailer:', error);
+    return null;
+  }
+};
+
+// Function to fetch banner url based on movie id
+export const fetchBannerUrl = async (id) => {
+  const API_URL = `${BASE_URL}/movie/${id}/images?api_key=${API_KEY}`;
+  try {
+    let response = await fetch(API_URL);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+
+    //calculte the optimal img size to use based on viewport width
+    const viewportWidth = document.documentElement.clientWidth;
+    let imgSize = BANNER_SIZES[BANNER_SIZES.length - 1][1]; //default to biggest img size
+    for (let i = 0; i < BANNER_SIZES.length; i++) {
+      if (viewportWidth <= BANNER_SIZES[i][0]) {
+        imgSize = BANNER_SIZES[i][1];
+        break;
+      }
+    }
+
+    //return the first banner url seen from the request
+    return IMG_BASE_URL + imgSize + data.backdrops[0].file_path;
+
+    // return data.results;
+  } catch (error) {
+    console.error('Error fetching movie banner url:', error);
     return null;
   }
 };
