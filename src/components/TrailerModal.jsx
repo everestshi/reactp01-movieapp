fd766a2dce022547862cc8a1872ad694f0388fd2
alexfang324 <106351391+alexfang324@users.noreply.github.com>
2024-02-11 16:18:46 -0800

    Video (#11)
    * debugging
    
    * stable version, fixed trailer modal size issue
    
    * changed website theme to black
    
    * added movie banner background to movie detail page

--- src/components/TrailerModal.jsx
+++ src/components/TrailerModal.jsx
@@ -3,16 +3,19 @@ import Button from "react-bootstrap/Button";
 
 function TrailerModal({ trailerUrl, showModal, setShowModal }) {
   const trailerComponent = trailerUrl ? (
-    <>
+    <div className="youtube-container">
+
       <Button className="modal-close-btn" onClick={() => setShowModal(false)}>
         Close
       </Button>
+
       <iframe
-        src={trailerUrl + "?autoplay=1"}
-        allow="autoplay"
+
+        src={trailerUrl + '&autoplay=1'}
+        allow="autoplay; accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
       ></iframe>
-    </>
+    </div>
   ) : (
     ""
   );
