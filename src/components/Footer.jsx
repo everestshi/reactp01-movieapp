ba12a733498690bda0c049d5b121ffca2f67fad4
Amanda Mar <msamandamar@gmail.com>
2024-02-13 20:27:29 -0800

    initial nav bar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,7 @@ import { getYear } from '../utilities/getDates';
 const Footer = ({ copyright, author }) => (
 	<footer>
         <p>&copy; {copyright} {author}</p>
+        <a href='#'>Top of Page</a>
     </footer>
 );
 
