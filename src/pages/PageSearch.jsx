4ddd0756b1753e49e09c39f68d8c9215f40d984b
Amanda Mar <msamandamar@gmail.com>
2024-02-18 19:20:41 -0800

    initial search page styling

--- src/pages/PageSearch.jsx
+++ src/pages/PageSearch.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { searchMovies } from "../data/tmdb-data";
 import MovieThumbnail from "../components/MovieThumbnail";
+import { Link } from "react-router-dom";
 
 const SearchPage = () => {
   const location = useLocation();
@@ -27,21 +28,39 @@ const SearchPage = () => {
   }, [searchQuery]); // Execute the effect whenever searchQuery changes
 
   return (
+    <main>
     <div className="search-page">
-      <div className="search-container">
+      <div className="search-container page-search">
         <input
           type="text"
           value={searchQuery}
+          className="search-term-page"
           onChange={(e) => setSearchQuery(e.target.value)}
           placeholder="Search for movies..."
         />
       </div>
-      <div className="search-results">
-        {searchResults.map((movie) => (
-          <MovieThumbnail key={movie.id} movieObj={movie} />
-        ))}
-      </div>
+      <div className="title-bottom-border"></div>
+      {searchResults.length < 1 ? (
+        <div className="border-container">
+          <img
+            className="popcorn-img"
+            src="../../public/assets/images/CuteDetective.png"
+            alt="Cute Detective"
+          />
+          <div className="empty-description">
+            <p>Looks like you haven't added a search term yet!</p>
+            <p>Start typing in the search bar to begin your search.</p>
+          </div>
+        </div>
+      ) : (
+        <div className="search-results">
+          {searchResults.map((movie) => (
+            <MovieThumbnail key={movie.id} movieObj={movie} />
+          ))}
+        </div>
+      )}
     </div>
+    </main>
   );
 };
 
